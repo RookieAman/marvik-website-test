0514164da4fbe461c01ab84e2053ac61dccfc3cf
RookieAman <rookieaman@gmail.com>
2022-11-12 00:01:35 +0100

    overflow testing 5

--- components/Work.js
+++ components/Work.js
@@ -396,7 +396,7 @@ export default function Work() {
         </div>
       </div> */}
 
-      {/* <div className="w-full flex justify-center items-center mb-[800px] sm:mb-[500px]">
+      <div className="w-full flex justify-center items-center mb-[800px] sm:mb-[500px]">
         <div className="marvikCircle relative w-[160px] md:w-[360px] h-[160px] md:h-[360px] rounded-[100%] flex justify-center items-center mr-[30px] md:mr-[100px]">
           <MarvikGlyph className="w-[70px] md:w-[170px] absolute" />
           <div
@@ -424,7 +424,7 @@ export default function Work() {
             <span>EXPLORE OUR WORK</span>
           </a>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
