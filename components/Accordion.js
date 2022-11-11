70b09738d9f21710cdbe41a6318ebe17df23b174
RookieAman <rookieaman@gmail.com>
2022-11-11 13:51:15 +0100

    project reestructure

---
+++ components/Accordion.js
@@ -0,0 +1,83 @@
+import { useState, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import getRelativeCoordinates from "../hooks/getRelativeCoordinates";
+import Image from "next/image";
+
+export const Accordion = ({
+  id,
+  expanded,
+  setExpanded,
+  title,
+  desc,
+  hoverImg,
+}) => {
+  const isOpen = id === expanded;
+
+  const [mousePosition, setMousePosition] = useState({});
+  const boxRef = useRef();
+  const handleMouseMove = (e) => {
+    setMousePosition(getRelativeCoordinates(e, boxRef.current));
+  };
+
+  return (
+    <motion.div
+      ref={boxRef}
+      className="px-5 lg:px-[10vw] lg:pb-[200px] cursor-pointer pt-6 relative"
+      initial={false}
+      animate={{ fontWeight: isOpen ? 700 : 0 }}
+      onMouseEnter={() => setExpanded(isOpen ? false : id)}
+      onMouseMove={(e) => handleMouseMove(e)}
+    >
+      <motion.h2
+        animate={{ color: isOpen ? "#791BF5" : "#000" }}
+        className="font-biotiflight text-4xl lg:text-7xl 2xl:text-9xl max-w-[700px] mb-12 flex"
+      >
+        {title}
+        {mousePosition.x && (
+          <motion.div
+            className="absolute hidden md:block md:w-[400px] md:h-[600px] 2xl:w-[800px] 2xl:h-[1000px]"
+            src={hoverImg}
+            initial={{ opacity: 0 }}
+            animate={{
+              opacity: isOpen && mousePosition.x ? 1 : 0,
+              x: mousePosition.x,
+              y: mousePosition.y - 500,
+            }}
+          >
+            <Image
+              className="pointer-events-none"
+              src={hoverImg}
+              alt=""
+              width={800}
+              height={1000}
+            />
+          </motion.div>
+        )}
+      </motion.h2>
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            className="w-full hidden md:block"
+          >
+            <span className="example font-light hidden md:block text-xl">
+              {desc}
+            </span>
+            <motion.div
+              initial={{ width: 0 }}
+              animate={{ width: "100%" }}
+              transition={{ delay: 0.2 }}
+              className="md:mt-6 xl:mt-24 line w-full border-b-2 border-black hidden md:block"
+            ></motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
--- components/Services.js
+++ components/Services.js
@@ -1,41 +1,11 @@
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import { useState, useRef } from "react";
-
-function getRelativeCoordinates(event, referenceElement) {
-  const position = {
-    x: event.pageX,
-    y: event.pageY,
-  };
-
-  const offset = {
-    left: referenceElement.offsetLeft,
-    top: referenceElement.offsetTop,
-    width: referenceElement.clientWidth,
-    height: referenceElement.clientHeight,
-  };
-
-  let reference = referenceElement.offsetParent;
-
-  while (reference) {
-    offset.left += reference.offsetLeft;
-    offset.top += reference.offsetTop;
-    reference = reference.offsetParent;
-  }
-
-  return {
-    x: position.x - offset.left,
-    y: position.y - offset.top,
-    width: offset.width,
-    height: offset.height,
-    centerX: (position.x - offset.left - offset.width / 2) / (offset.width / 2),
-    centerY:
-      (position.y - offset.top - offset.height / 2) / (offset.height / 2),
-  };
-}
+import { Accordion } from "./Accordion";
 
 export default function Services() {
   const [expanded, setExpanded] = useState();
+  const { title, desc, img } = accordionIds;
 
   return (
     <div className="px-5 md:px-14 mb-[700px]">
@@ -90,86 +60,6 @@ export default function Services() {
     </div>
   );
 }
-
-export const Accordion = ({
-  id,
-  expanded,
-  setExpanded,
-  title,
-  desc,
-  hoverImg,
-}) => {
-  const isOpen = id === expanded;
-
-  const [mousePosition, setMousePosition] = useState({});
-  const boxRef = useRef();
-  const handleMouseMove = (e) => {
-    setMousePosition(getRelativeCoordinates(e, boxRef.current));
-    console.log(mousePosition);
-  };
-
-  return (
-    <motion.div
-      ref={boxRef}
-      className="px-5 lg:px-[10vw] lg:pb-[200px] cursor-pointer pt-6 relative"
-      initial={false}
-      animate={{ fontWeight: isOpen ? 700 : 0 }}
-      onMouseEnter={() => setExpanded(isOpen ? false : id)}
-      onMouseMove={(e) => handleMouseMove(e)}
-    >
-      <motion.h2
-        animate={{ color: isOpen ? "#791BF5" : "#000" }}
-        className="font-biotiflight text-4xl lg:text-7xl 2xl:text-9xl max-w-[700px] mb-12 flex"
-      >
-        {title}
-        {mousePosition.x && (
-          <motion.div
-            className="absolute hidden md:block md:w-[400px] md:h-[600px] 2xl:w-[800px] 2xl:h-[1000px]"
-            src={hoverImg}
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: isOpen && mousePosition.x ? 1 : 0,
-              x: mousePosition.x,
-              y: mousePosition.y - 500,
-            }}
-          >
-            <Image
-              className="pointer-events-none"
-              src={hoverImg}
-              alt=""
-              width={800}
-              height={1000}
-            />
-          </motion.div>
-        )}
-      </motion.h2>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.div
-            initial="collapsed"
-            animate="open"
-            exit="collapsed"
-            variants={{
-              open: { opacity: 1, height: "auto" },
-              collapsed: { opacity: 0, height: 0 },
-            }}
-            className="w-full hidden md:block"
-          >
-            <span className="example font-light hidden md:block text-xl">
-              {desc}
-            </span>
-            <motion.div
-              initial={{ width: 0 }}
-              animate={{ width: "100%" }}
-              transition={{ delay: 0.2 }}
-              className="md:mt-6 xl:mt-24 line w-full border-b-2 border-black hidden md:block"
-            ></motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </motion.div>
-  );
-};
 
 const accordionIds = [
   {
