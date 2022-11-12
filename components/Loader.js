c4df9ec23ed68dbf9564d0fa3dc6cae2ca609c31
RookieAman <rookieaman@gmail.com>
2022-11-12 16:09:27 +0100

    blackscreen test

--- components/Loader.js
+++ components/Loader.js
@@ -27,7 +27,7 @@ const AnimatedLetters = ({ title, customDelay, setLoading }) => (
     initial="initial"
     animate="animate"
     custom={customDelay}
-    onAnimationComplete={setLoading ? () => setLoading(false) : ""}
+    // onAnimationComplete={setLoading ? () => setLoading(false) : ""}
   >
     {[...title].map((letter, i) => (
       <motion.span
@@ -45,7 +45,7 @@ const loader = ({ setLoading }) => {
   return (
     <section className="main bg-black h-[100vh] w-full">
       <div className="px-5 md:pt-[16vw] flex justify-center pb-[7vw] pt-[40vw]">
-        <h1 className="relative md:text-[4.5vw] md:tracking-[.10vw] md:leading-[4.2vw] text-3xl tracking-tight font-biotiflight text-white  max-w-[430px] md:max-w-none">
+        <h1 className="relative md:text-[4.5vw] md:tracking-[.10vw] md:leading-[4.2vw] text-3xl tracking-tight font-biotiflight text-white">
           <div className="line">
             <AnimatedLetters customDelay={0.4} title={`Empresa,\u00A0`} />
             <AnimatedLetters customDelay={1.5} title={`futuro.`} />
